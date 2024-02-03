e78b4cd2a7da94f9be7364ece4c0b19c5d9ecbc5
Himanshupoojary <110303027+Himanshupoojary@users.noreply.github.com>
2024-02-03 15:05:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ server.js
@@ -0,0 +1,140 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import mongoose, { Schema, mongo } from "mongoose";
+import { strict } from "assert";
+import bodyParser from "body-parser";
+import dotenv from "dotenv"
+import pkg from 'body-parser';
+
+
+
+
+
+const { json } = pkg;
+dotenv.config()
+const app = express() ;
+const port =process.env.PORT || 5000;
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.use(bodyParser.json())
+const username = process.env.MONGODB_USERNAME
+const password = process.env.MONGODB_PASSWORD
+
+
+
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.suqnipw.mongodb.net/LoginRegDB`)
+
+
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
+app.get("/p1",(req,res)=>{
+    res.render("portfolio-1/p-1index.ejs",{
+        title:"Rahul Banger",
+        name: "Saketh Parimi",
+        skillarray:{
+            skill_name:"web dev",
+            skill_info:"Its been a long dayyy "
+           }
+
+    })
+})
+app.get("/p2",(req,res)=>{
+    res.render("portfolio-2/p-2index.ejs")
+})
+
+app.get("/p3",(req,res)=>{
+    res.render("portfolio-3/p-3index.ejs")
+})
+
+
+app.get("/addinfo",(req,res)=>{
+    res.sendFile(__dirname + "/views/addinfo.html")
+})
+//
+// var popupS = require('popups');
+ 
+// popupS.alert({
+//     content: 'Hello World!'
+// });
+
+
+//model for mongoose
+
+// const regscema=new mongoose.Schema({
+//     name:String,
+//     email:String,
+//     password:String
+// })
+
+// const Registration = mongoose.model("Regsitration",regscema)
+
+
+//mongoose testing 
+
+// const fruitshema = new Schema ({
+//     name:String,
+//     rating:Number
+// })
+
+// const Fruit = mongoose.model("Fruit",fruitshema)
+
+// const fruit1= new Fruit({name:"mahesh" ,rating:9 })
+// fruit1.save()
+
+const regscema=new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String
+})
+
+const Registration = mongoose.model("Regsitration",regscema)
+
+app.get("/success",(req,res)=>{
+    res.sendFile(__dirname + "/views/success.html")
+})
+app.get("/error",(req,res)=>{
+    res.sendFile(__dirname + "/views/error.html")
+})
+app.get("/register",(req,res)=>{
+    res.render("regist.ejs")
+})
+
+app.post("/register",async (req,res)=>{ 
+    try {
+        
+        const {name,email,password} = req.body
+        const exist = await Registration.findOne({email:email})
+        if (!exist)  {
+                const user = new Registration ({
+                name:name,
+                email:email,
+                password:password
+        })      
+                await user.save()
+                res.redirect("/success")}
+        else {
+            await res.render("regist.ejs",{alreadyexist:true})
+
+        }
+         
+        
+        
+    } catch (error) {
+        res.redirect("/error")
+    }
+})
+  
+
+
+app.listen(port,(err)=>{
+    if (err) {
+        console.log('Error sir')
+    }else{
+        console.log(`Listening at port ${port}`)
+    }
+    
+})
